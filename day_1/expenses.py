fffa8f247bbf182aa01530d1ad14ddd912d3297b
Alex Clay <alex.clay@logicsupply.com>
2020-12-01 09:29:03 -0500

    Day one

---
+++ day_1/expenses.py
@@ -0,0 +1,19 @@
+lines = []
+with open('input.txt', 'r') as inp_file:
+    for line in inp_file:
+        lines.append(int(line.strip()))
+
+print('--- PART 1 ---')
+for i in range(len(lines)):
+    for j in range(i, len(lines)):
+        if lines[i] +lines[j] == 2020:
+            print(lines[i] + lines[j])
+            print(lines[i] * lines[j])
+
+print('--- PART 2 ---')
+for i in range(len(lines)):
+    for j in range(i, len(lines)):
+        for k in range(j, len(lines)):
+            if lines[i] +lines[j] + lines[k] == 2020:
+                print(lines[i] + lines[j] + lines[k])
+                print(lines[i] * lines[j] * lines[k])
